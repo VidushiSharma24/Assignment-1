0e0b64d1d90a6ba97c39b2daa0dda769650ec38e
Vidushisharma04 <23egjcs830@gitjaipur.com>
2025-11-07 11:57:28 +0530

    Updated Assignment 1 files for PR

--- src/ProblemStatement_07/Circle.java
+++ src/ProblemStatement_07/Circle.java
@@ -2,7 +2,7 @@ package ProblemStatement_07;
 
 public class Circle extends Shape{
     private double radius;
-    private final double PI = 3.14159; // manually defined
+    private final double PI = 3.14159; // manually defined final
 
     public Circle(double radius) {
         this.radius = radius;
